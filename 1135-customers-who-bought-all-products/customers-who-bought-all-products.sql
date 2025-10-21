b8b06caa9052c227ad7fd82b7b1be22f8a0e0753
shorya007 <kumarshourya7781@gmail.com>
2025-10-21 23:20:33 +0530

    Time: 580 ms (36.89%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -2,3 +2,6 @@
 select customer_id from customer
 group by customer_id
 having count(distinct product_key) = (select count(distinct product_key)from product) 
+ # count(distinct product_key) ye dekhega customer_id mein kitne product hain
+ # select count(distinct product_key)from product ye product table mein total product calculate krega 
+ # aur jab dono ka value same ho jaega toh bas whi group by karke customer_id mein bas customer_id ka value return kr dega 
